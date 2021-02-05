f34a43b97d76f3ca1d992ec484143a85cd5bfef4
DmitriyKonnov <dimon170893@gmail.com>
2021-02-05 17:05:40 +0300

    added lesson 4

---
+++ lesson 4()/lesson_4.js
@@ -0,0 +1,18 @@
+
+// // Создайте button.b-1 - при нажатии на него выводите alert с номером задачи.
+
+let button = document.querySelector('.btn-1');
+
+button.addEventListener('click', () => { alert('Задача №1') });
+
+// Создайте input.b-2 type=button - при нажатии на него выводите alert с номером задачи. Номер задачи просто пропишите вручную.
+
+let button_2 = document.querySelector('.btn-2');
+
+button_2.addEventListener('click', () => { alert('Задача №2') });
+
+// Создайте p.p-3 - при нажатии на него выводите alert с номером задачи.
+
+let button_3 = document.querySelector('.btn-3');
+
+button_3.addEventListener('click', () => { alert('Задача №3') });
